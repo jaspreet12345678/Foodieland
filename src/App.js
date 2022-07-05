b34677925a771564972b351ca3f3217ceb718086
jaspreet12345678 <jaspreet.mangoit@gmail.com>
2022-07-05 18:12:33 +0530

    i

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import Cont from "./contact/input";
-// import Contact from "./contact";
 import Index from "./components/Home";
 
 function App() {
   return (
     <>
       <Index/>
-      <Cont />
     </>
   );
 }
