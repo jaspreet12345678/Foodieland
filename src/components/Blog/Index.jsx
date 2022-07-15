8d26ff5abe2c2c311e9f40fdf0edb94ece94b780
jaspreet12345678 <jaspreet.mangoit@gmail.com>
2022-07-15 18:15:49 +0530

    d

--- src/components/Blog/Index.jsx
+++ src/components/Blog/Index.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Footer from '../usable/Footer';
-import Static from '../usable/Static';
-import BlogHeader from './BlogHeader';
+import React from "react";
+import Footer from "../usable/Footer";
+import Header from "../usable/Header";
+import Static from "../usable/Static";
+import BlogHeader from "./BlogHeader";
 
 function BlogLists(props) {
-    return (
-        <>
-            <BlogHeader />
-            <Static />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <BlogHeader />
+      <Static />
+      <Footer />
+    </>
+  );
 }
 
 export default BlogLists;
