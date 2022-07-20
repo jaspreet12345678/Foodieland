498a1e90924e6fb29ecc94334f41954168eb2848
jaspreet12345678 <jaspreet.mangoit@gmail.com>
2022-07-20 16:04:08 +0530

    Recipe Update

--- src/utils/blogSer.js
+++ src/utils/blogSer.js
@@ -3,21 +3,25 @@ import axios from "axios";
 const apiEndPoint = "https://foodielandnod.herokuapp.com/api/";
 
 export const getAllBlogs = () => {
- return axios.get(apiEndPoint + 'getAllBlog');
-}
+  return axios.get(apiEndPoint + "getAllBlog");
+};
 
-export const getAllPopularBlogs =() => {
-    return axios.get(apiEndPoint + 'popularBlogs');
-}
+export const getAllPopularBlogs = () => {
+  return axios.get(apiEndPoint + "popularBlogs");
+};
 
 export const getSearchQuery = (query) => {
-    return axios.get(apiEndPoint + 'searchBlog?key=' + query );
-}
+  return axios.get(apiEndPoint + "searchBlog?key=" + query);
+};
 
-export const getSearch = (query) => {
-    return axios.get(apiEndPoint + 'searcRecipe?key' + query );
-}
+export const getSearchReceipe = (query) => {
+  return axios.get(apiEndPoint + "searcRecipe?key=" + query);
+};
 
 export const getOneBlogPost = (blogId) => {
-    return axios.get(apiEndPoint + 'getBlog?id=' + blogId);
-}
+  return axios.get(apiEndPoint + "getBlog?id=" + blogId);
+};
+
+export const getOneReceipePost = (receipeId) => {
+  return axios.get(apiEndPoint + "recipeDetails?id=" + receipeId);
+};
